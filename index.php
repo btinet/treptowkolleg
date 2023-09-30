2ae213f4fe9cd70e400e3ce89fffe074985b4a51
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-09-30 12:16:46 +0200

    update docs

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if (isset($_GET['file']) and !empty($_GET['file']) ) {
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/base16/solar-flare.min.css" integrity="sha512-+qcmYgdPNX/uKWgS45T0StFAPNTOaHJlP/AB2IpikjqT8UwNY/HhwzbqnezTWhB3ib6rCAXPhPOiBAfGYGBK/g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github.min.css" integrity="sha512-0aPQyyeZrWj9sCA46UlmWgKOP0mUipLQ6OZXu8l4IcAmD2u31EPEy9VcIMvl7SoAaKe8bLXZhYoMaE/in+gcgA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
 
