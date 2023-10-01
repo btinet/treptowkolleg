4234ec5eecf003f85ab92b0d1b1b78ab3d8834cb
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-10-01 18:20:58 +0200

    update docs

--- index.php
+++ index.php
@@ -123,26 +123,20 @@ $entries = dirToArray('./docs');
             /* make space  */
             margin: 5px 0;
             padding: 1.75rem 0 1.75rem 1rem;
-            border-radius: 10px;
         }
 
         pre button {
             position: absolute!important;
-            top: 35px;
-            right: 5px;
+            top: 40px;
+            right: 10px;
 
             font-size: 0.9rem;
             padding: 0.15rem;
-            background-color: #828282;
-
-            border: ridge 1px #7b7b7c;
-            border-radius: 5px;
-            text-shadow: #c4c4c4 0 0 2px;
         }
 
         pre button:hover {
             cursor: pointer;
-            background-color: #bcbabb;
+            background-color: #eaeef2;
         }
 
 
@@ -321,7 +315,7 @@ $entries = dirToArray('./docs');
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js"></script>
 <script>hljs.highlightAll();</script>
 <script>
-    const copyButtonLabel = "Copy Code";
+    const copyButtonLabel = "<i class='fas fa-copy'></i>";
 
     // use a class selector if available
     let blocks = document.querySelectorAll("pre");
