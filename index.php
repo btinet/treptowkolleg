a2338d555d740fecdb17d39e1cbf79e704b56512
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-10-01 21:36:43 +0200

    update docs

--- index.php
+++ index.php
@@ -350,7 +350,7 @@ $entries = dirToArray('./docs');
 
     document.addEventListener("DOMContentLoaded", function(event) {
         const copyButtonLabel = "<i class='fas fa-copy'></i>";
-        const copiedButtonLabel = "<i class='fas fa-check'></i>";
+        const copiedButtonLabel = "<i class='fas fa-check'></i> kopiert";
 
         let button = document.getElementById("myBtn");
 
