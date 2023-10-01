6851e921e06e758b9be4c089ca7b18c64e755194
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-10-01 14:52:41 +0200

    update docs

--- index.php
+++ index.php
@@ -118,15 +118,6 @@ $entries = dirToArray('./docs');
 </head>
 <body>
 
-<div class="pf-v5-c-back-to-top">
-    <a class="pf-v5-c-button pf-m-primary" href="#">
-        nach oben
-        <span class="pf-v5-c-button__icon pf-m-end">
-      <i class="fas fa-angle-up" aria-hidden="true"></i>
-    </span>
-    </a>
-</div>
-
 <header class="pf-v5-c-masthead" id="card-view-basic-example-masthead">
     <div class="pf-v5-c-masthead__content">
         <div
@@ -288,6 +279,15 @@ $entries = dirToArray('./docs');
             </div>
         </div>
     </section>
+    <div class="pf-v5-c-back-to-top">
+        <a class="pf-v5-c-button pf-m-primary" href="#">
+            nach oben
+            <span class="pf-v5-c-button__icon pf-m-end">
+      <i class="fas fa-angle-up" aria-hidden="true"></i>
+    </span>
+        </a>
+    </div>
+</main>
 
 
 
