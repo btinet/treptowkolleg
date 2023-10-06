1848f410612bee3163ba9b7986f776b624213daa
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-10-06 22:21:06 +0200

    update docs

--- index.php
+++ index.php
@@ -222,7 +222,7 @@ $entries = dirToArray('./docs');
             </div>
         </div>
     </header>
-    <div class="pf-v5-c-page__sidebar" id="sidebar" style="transform: translateX(-100%)">
+    <div class="pf-v5-c-page__sidebar" id="sidebar" style="transform: translateX(0)">
         <div class="pf-v5-c-page__sidebar-body">
             <nav class="pf-v5-c-nav" id="page-demo-sticky-top-section-group-primary-nav" aria-label="Global">
                 <div
